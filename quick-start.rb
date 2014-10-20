6c03555395b7c820ead1f3cdf4080f2b83c55d1a
James Heywood <jdheywood@yahoo.co.uk>
2014-10-20 16:16:55 +0100

    Added selenium-webdriver examples and watir-webdriver example

---
+++ quick-start.rb
@@ -0,0 +1,11 @@
+require "selenium-webdriver"
+browser = Selenium::WebDriver.for :firefox
+browser.get "https://www.google.com/"
+p browser.current_url
+browser.find_element(link_text: "Use Google.com").click
+browser.find_element(link_text: "Images").click
+p browser.title
+browser.find_element(name: "q").send_keys "book"
+browser.find_element(name: "btnG").click
+p browser.find_elements(tag_name: "img").size
+browser.close
