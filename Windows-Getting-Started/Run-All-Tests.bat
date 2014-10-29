0bf1c0c119db17356a9c9200e406be2b1927e504
Tony Das <tony.das@amido.com>
2014-10-29 16:23:47 +0000

    added to Run-All-Tests.bat a call to Firefox with results once test completes.

--- Windows-Getting-Started/Run-All-Tests.bat
+++ Windows-Getting-Started/Run-All-Tests.bat
@@ -1,4 +1,3 @@
 ECHO Running All Cucumber Tests
 CALL cucumber ../Features --format Cucumber::Formatter::Html --out .\test-results\results.html
-ECHO Tests completed
-PAUSE
+CALL "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" .\test-results\results.html
