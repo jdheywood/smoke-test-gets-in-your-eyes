fc77fff893d68786f30e3daf2833e2b7e5682cc5
Tony Das <tony.das@amido.com>
2014-10-29 16:15:27 +0000

    added screenshot-on-failure facility, embedding into HTML report also added syntax package for syntax highlighting

--- Features/support/env.rb
+++ Features/support/env.rb
@@ -6,6 +6,13 @@ Before do |scenario|
   @browser = browser
 end
 
+After do |scenario|
+  if(scenario.failed?)
+    encoded_img = @browser.screenshot_as(:base64)
+    embed("data:image/png;base64,#{encoded_img}",'image/png')
+  end
+end
+
 at_exit do
   browser.quit
 end
