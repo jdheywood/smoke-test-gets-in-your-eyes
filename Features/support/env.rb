29e30ac1b3994172429020b931d38032f2800a28
Tony Das <tony.das@amido.com>
2014-10-29 17:03:14 +0000

    output page source on failing scenario step from webDriver

--- Features/support/env.rb
+++ Features/support/env.rb
@@ -9,7 +9,12 @@ end
 After do |scenario|
   if(scenario.failed?)
     encoded_img = @browser.screenshot_as(:base64)
+
     embed("data:image/png;base64,#{encoded_img}",'image/png')
+
+    pageSource = File.open("./test-results/#{ scenario.name } pageSource.html","w")
+    pageSource.puts @browser.page_source
+    pageSource.close
   end
 end
 
