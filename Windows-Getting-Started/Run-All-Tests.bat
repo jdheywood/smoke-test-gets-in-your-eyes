632028bfb6982fd0e50d5bfed6a68bfaaf9ce2d3
Tony Das <tony.das@amido.com>
2014-10-31 12:39:22 +0000

    added bundler exec to Run-All-Tests.bat

--- Windows-Getting-Started/Run-All-Tests.bat
+++ Windows-Getting-Started/Run-All-Tests.bat
@@ -1,3 +1,3 @@
 ECHO Running All Cucumber Tests
-CALL cucumber ../Features --format Cucumber::Formatter::Html --out .\test-results\results.html
+CALL bundler exec cucumber ../Features --format Cucumber::Formatter::Html --out .\test-results\results.html
 CALL "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" .\test-results\results.html
